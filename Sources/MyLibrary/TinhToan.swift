8b06280b5849d0177fbc7514cd44773050cfdadf
TuanPQ <quyen.nguyen4@ntq-solution.com.vn>
2022-10-24 17:32:23 +0700

    Update

---
+++ Sources/MyLibrary/TinhToan.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Quyen, Nguyen Huu  on 21/10/2022.
+//
+
+import Foundation
+
+public class TinhTuoi {
+    public init() {
+        
+    }
+    
+    public func tinhTuoi(namSinh: Int) -> Int {
+        return 2022 - namSinh
+    }
+}
